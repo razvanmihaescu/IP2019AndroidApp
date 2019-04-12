e13282e79131a7320d5248d23bd40b6918eca271
mihaescu <razvanmihaescu@student.upt.ro>
2019-04-13 01:38:50 +0300

    Added Login UI.

--- app/src/main/java/com/example/ip2019androidapp/LoginActivity.java
+++ app/src/main/java/com/example/ip2019androidapp/LoginActivity.java
@@ -1,14 +1,13 @@
 package com.example.ip2019androidapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
