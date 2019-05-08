114f3cd728cfd0eb7f403114c6d3f8de977df263
mihaescu <razvan.mihaescu@student.upt.ro>
2019-05-08 14:11:45 +0300

    Integrated the BT Connectivity feature.

--- app/src/main/java/com/example/ip2019androidapp/LoggedActivity.java
+++ app/src/main/java/com/example/ip2019androidapp/LoggedActivity.java
@@ -1,12 +1,14 @@
 package com.example.ip2019androidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class LoggedActivity extends AppCompatActivity {
 
-
+    Button btn_remote_control,btn_local_control,btn_BTconnect;
     public static String drName=null;
 
     @Override
@@ -14,10 +16,16 @@ public class LoggedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged);
         drName = getIntent().getStringExtra(drName);
-//        Button btn_remote_control =(Button) findViewById(R.id.btnAutomat);
-//        Button btn_local_control = (Button) findViewById(R.id.btnLocal);
-//        btn_local_control.setActivated(false);
-//        btn_remote_control.setActivated(false);
 
+        btn_BTconnect=(Button) findViewById(R.id.btnBTconnect);
+        btn_remote_control =(Button) findViewById(R.id.btnAutomat);
+        btn_local_control = (Button) findViewById(R.id.btnLocal);
+        btn_local_control.setClickable(false);
+        btn_remote_control.setClickable(false);
+
+    }
+    public void doLaunchBT_ConnectingProcedure(View view) {
+    Intent intent = new Intent(this,DeviceList.class);
+    startActivity(intent);
     }
 }
