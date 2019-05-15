29d038f6e6f09748d2a02cc6bbb20103ce0f8d5a
mihaescu <razvan.mihaescu@student.upt.ro>
2019-05-15 11:43:14 +0300

    Integrated the MD5 Encrypting feature. Tested. Working.

--- app/src/main/java/com/example/ip2019androidapp/LoggedActivity.java
+++ app/src/main/java/com/example/ip2019androidapp/LoggedActivity.java
@@ -5,17 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoggedActivity extends AppCompatActivity {
 
     Button btn_remote_control,btn_local_control,btn_BTconnect;
+    TextView mTVTitle;
     public static String drName=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged);
-        drName = getIntent().getStringExtra(drName);
+        drName = getIntent().getStringExtra("drName");
+
+        mTVTitle = findViewById(R.id.actLoggedText);
+        mTVTitle.setText(getString(R.string.drWelcome) + drName);
 
         btn_BTconnect=(Button) findViewById(R.id.btnBTconnect);
         btn_remote_control =(Button) findViewById(R.id.btnAutomat);
--- app/src/main/java/com/example/ip2019androidapp/MainActivity.java
+++ app/src/main/java/com/example/ip2019androidapp/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -19,32 +22,33 @@ import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static boolean isLogged=false;
+    public static boolean isLogged = false;
     Button btnLogin;
-    EditText txtUser,txtPass;
+    EditText txtUser, txtPass;
 
     Connection con;
-    String us,pass,db,ip;
+    String us, pass, db, ip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogin =  (Button)findViewById(R.id.btnLogin);
-        txtUser = (EditText)findViewById(R.id.actLogin_user);
-        txtPass = (EditText)findViewById(R.id.actLogin_pass);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        txtUser = (EditText) findViewById(R.id.actLogin_user);
+        txtPass = (EditText) findViewById(R.id.actLogin_pass);
 
-//        ip="35.225.197.231";
-//        pass="8Barosani";
-//        us="echipa";
-//        db="ICARE";
+        ip="35.225.197.231";
+        pass="8Barosani";
+        us="echipa";
+        db="ICARE";
 
         //      localhost
         //From the emulator, 127.0.0.1 refers to the emulator itself - not your local machine. You need to use ip 10.0.2.2, which is bridged to your local machine.
-        ip="10.0.2.2";  //
-        pass="root";
-        us="root";
-        db="ICARE";
+//        ip = "10.0.2.2";  //
+//        pass = "root";
+//        us = "root";
+//        db = "ICARE";
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
 
                 CheckLogin checkLogin = new CheckLogin();
                 checkLogin.execute("");
+
+                //just for testing on my smartphone
+//                Intent i = new Intent(MainActivity.this, LoggedActivity.class);
+//                i.putExtra("drName",txtUser.getText().toString());
+//                startActivity(i);
             }
         });
     }
@@ -63,31 +72,23 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... strings) {
             String usernam = txtUser.getText().toString();
             String passw = txtPass.getText().toString();
-            if(usernam.trim().equals("")||passw.trim().equals(""))
-            {
+            if (usernam.trim().equals("") || passw.trim().equals("")) {
                 z = "Please enter Username and Pass";
-            }
-            else
-            {
-                con = connectionCreate(us,pass,db,ip);
-                if(con==null)
-                {
-                    z= "Check your internet";
-                }
-                else
-                {
-                    String query = "select * from MEDICI where Username= '"+usernam.toString()+"' and Password='"+passw.toString()+"'";
+            } else {
+                con = connectionCreate(us, pass, db, ip);
+                if (con == null) {
+                    z = "Check your internet";
+                } else {
+                    String query = "select * from MEDICI where Username= '" + usernam.toString() + "' and Password='" + getMd5(passw) + "'";
                     try {
-                        Statement stmt=con.createStatement();
-                        ResultSet rs= stmt.executeQuery(query);
-                        if(rs.next())
-                        {
-                            z= "Login Successful";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+                        if (rs.next()) {
+                            z = "Login Successful";
                             isLogged = true;
                             con.close();
-                        }
-                        else {
-                            z="Invalid credentials";
+                        } else {
+                            z = "Invalid credentials";
                             isLogged = false;
                         }
                     } catch (SQLException e) {
@@ -102,12 +103,11 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
-            if(isLogged)
-            {
-                Toast.makeText(MainActivity.this,"Login Successfull",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            if (isLogged) {
+                Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
                 Intent i = new Intent(MainActivity.this, LoggedActivity.class);
-                i.putExtra("drName",txtUser.getText().toString());
+                i.putExtra("drName", txtUser.getText().toString());
                 startActivity(i);
             }
         }
@@ -121,17 +121,41 @@ public class MainActivity extends AppCompatActivity {
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            ConnectionURL = "jdbc:mysql://"+server+":3306/"+database;
-            connection = DriverManager.getConnection(ConnectionURL,user,password);
-        }
-        catch (SQLException se)
-        {
-            Log.e("error here 1 : " , se.getMessage());
+            ConnectionURL = "jdbc:mysql://" + server + ":3306/" + database;
+            connection = DriverManager.getConnection(ConnectionURL, user, password);
+        } catch (SQLException se) {
+            Log.e("error here 1 : ", se.getMessage());
 
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.e("error here 3 : ", e.getMessage());
         }
         return connection;
+    }
+
+    public static String getMd5(String input) {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
